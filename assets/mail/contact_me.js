54ae2ce02620dda5471462a993ef9fd8073c5058
StevenSawant <trueace02@gmail.com>
2026-03-25 18:12:22 +0530

    Added API endpoint URL of the API gateway

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "{Insert your own AWS API Gateway Endpoint URL here}",
+                url: "https://d60bvvr2qg.execute-api.ap-south-1.amazonaws.com/default/portfolio-backend",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
